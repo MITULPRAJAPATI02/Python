5095e015348f5acd13e80a000ad8d15962270439
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-04-04 19:52:42 +0530

    Create IFloop1.py

---
+++ Conditions/IFloop1.py
@@ -0,0 +1,27 @@
+var1 = True
+
+if var1:
+    print(var1)
+else:
+    print(var1)
+
+# how to input in python
+# by default input function will return string
+# python2=>raw_input()->string input
+#             input()-<int input
+
+x = int(input("Enter Integer Value"))
+
+if x < 0:
+    x = 0
+    """print('Negative changed to zero')"""
+    if x == 0:
+        print('Zero')
+elif x == 1:
+    print('Single')
+    if x == 0:
+        print('Zero')
+else:
+    print('More')
+
+
