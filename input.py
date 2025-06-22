6621a845e247240d5f5ec204243400538409ea49
Mitul Prajapati <mbpfocus21@gmail.com>
2025-06-22 12:10:51 +0530

    basicpython

---
+++ input.py
@@ -0,0 +1,31 @@
+# name = input("enter your name ")
+# age = input("enter your age ")
+
+# print ("hello" + " "+ name  + "your age is" + " "+ age)
+
+
+
+# a = "3.14"
+
+# print(type(a))
+
+# a = float(a)
+# a = int(a)
+# print(type(a))
+
+
+
+# number1= int (input ("enter your N0.1"))
+# number2= int(input("enter your NO.2 "))
+
+# sum =  (number1) + (number2)
+
+# print("sum of two numbers is", number1, "+", number2, "=", sum)
+
+
+# side = int (input("enter size of side of square "))
+
+# area = side * side
+
+# print("area of square is", area)
+
