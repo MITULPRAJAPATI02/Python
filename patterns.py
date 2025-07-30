f71a0d81af8735820870ef9df53bce44282b68d4
MITUL PRAJAPATI <mbpfocus21@gmail.com>
2025-07-30 11:43:27 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -46,3 +46,55 @@
 # * * *
 # * *
 # *
+
+
+for i in range (1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print( )    
+    
+    
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print( )   
+
+# output
+
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 
+# 1 2 3 
+# 1 2 
+# 1 
+
+
+for i  in range   (1,6):
+    for j in range(i+1,0,-1):
+        print(j,end=" ")
+    print()  
+
+# output
+# 2 1 
+# 3 2 1 
+# 4 3 2 1 
+# 5 4 3 2 1 
+# 6 5 4 3 2 1 
+for i in range (5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()   
+        
+# output
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+
+
